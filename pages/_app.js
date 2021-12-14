548b4263160fb0a6ac59c52fb44be53166997db7
patriq <patriqdesigns@gmail.com>
2021-12-14 23:23:59 +0000

    Formatted and simplified the Custom App boilerplate code.

--- pages/_app.js
+++ pages/_app.js
@@ -1,77 +1,63 @@
-import ApolloClient from "apollo-boost";
-import { ApolloProvider } from "react-apollo";
-import App from "next/app";
-import { AppProvider } from "@shopify/polaris";
-import { Provider, useAppBridge } from "@shopify/app-bridge-react";
+import { Provider as AppBridgeProvider, useAppBridge } from "@shopify/app-bridge-react";
 import { authenticatedFetch } from "@shopify/app-bridge-utils";
 import { Redirect } from "@shopify/app-bridge/actions";
+import { AppProvider as PolarisProvider } from "@shopify/polaris";
 import "@shopify/polaris/dist/styles.css";
 import translations from "@shopify/polaris/locales/en.json";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
 
-function userLoggedInFetch(app) {
+const userLoggedInFetch = (app) => {
   const fetchFunction = authenticatedFetch(app);
-
   return async (uri, options) => {
     const response = await fetchFunction(uri, options);
-
-    if (
-      response.headers.get("X-Shopify-API-Request-Failure-Reauthorize") === "1"
-    ) {
+    if (response.headers.get("X-Shopify-API-Request-Failure-Reauthorize") === "1") {
       const authUrlHeader = response.headers.get(
         "X-Shopify-API-Request-Failure-Reauthorize-Url"
       );
-
       const redirect = Redirect.create(app);
       redirect.dispatch(Redirect.Action.APP, authUrlHeader || `/auth`);
       return null;
     }
-
     return response;
   };
-}
+};
 
-function MyProvider(props) {
+const CustomApolloProvider = ({ Component, ...props }) => {
   const app = useAppBridge();
 
   const client = new ApolloClient({
     fetch: userLoggedInFetch(app),
     fetchOptions: {
-      credentials: "include",
-    },
+      credentials: "include"
+    }
   });
-
-  const Component = props.Component;
 
   return (
     <ApolloProvider client={client}>
       <Component {...props} />
     </ApolloProvider>
   );
-}
-
-class MyApp extends App {
-  render() {
-    const { Component, pageProps, host } = this.props;
-    return (
-      <AppProvider i18n={translations}>
-        <Provider
-          config={{
-            apiKey: API_KEY,
-            host: host,
-            forceRedirect: true,
-          }}
-        >
-          <MyProvider Component={Component} {...pageProps} />
-        </Provider>
-      </AppProvider>
-    );
-  }
-}
-
-MyApp.getInitialProps = async ({ ctx }) => {
-  return {
-    host: ctx.query.host,
-  };
 };
 
-export default MyApp;
+const CustomApp = ({ Component, pageProps, host }) => {
+  return (
+    <PolarisProvider i18n={translations}>
+      <AppBridgeProvider
+        config={{
+          apiKey: API_KEY,
+          host: host,
+          forceRedirect: true
+        }}
+      >
+        <CustomApolloProvider Component={Component} {...pageProps} />
+      </AppBridgeProvider>
+    </PolarisProvider>
+  );
+};
+
+CustomApp.getInitialProps = async ({ ctx }) => ({
+  host: ctx.query.host
+});
+
+export default CustomApp;
