227007a572806f7ee3a3f960e42e1ec50dca11d3
patriq <patriqdesigns@gmail.com>
2021-12-19 21:37:44 +0000

    Changed query/mutation constant names.

--- context/ShopLocales.js
+++ context/ShopLocales.js
@@ -2,7 +2,7 @@ import { gql, useMutation, useQuery } from "@apollo/client";
 import React from "react";
 import { SHOP_LOCALES } from "../constants/locales";
 
-const GET_SHOP_LOCALES = gql`{
+const GET_SHOP_LOCALES_QUERY = gql`{
   shopLocales {
     locale
     primary
@@ -10,7 +10,7 @@ const GET_SHOP_LOCALES = gql`{
   }
 }`;
 
-const ENABLE_LOCALE = gql`
+const ENABLE_LOCALE_MUTATION = gql`
   mutation enableLocale($localeCode: String!) {
     shopLocaleEnable(locale: $localeCode) {
       userErrors {
@@ -27,7 +27,7 @@ const ENABLE_LOCALE = gql`
   }
 `;
 
-const DISABLE_LOCALE = gql`
+const DISABLE_LOCALE_MUTATION = gql`
   mutation disableLocale($localeCode: String!) {
     shopLocaleDisable(locale: $localeCode) {
       userErrors {
@@ -39,7 +39,7 @@ const DISABLE_LOCALE = gql`
   }
 `;
 
-const UPDATE_LOCALE = gql`
+const UPDATE_LOCALE_MUTATION = gql`
   mutation updateLocale($localeCode: String!, $shopLocale: ShopLocaleInput!) {
     shopLocaleUpdate(locale: $localeCode, shopLocale: $shopLocale) {
       userErrors {
@@ -67,28 +67,28 @@ export const useShopLocales = () => {
 };
 
 export const useEnableLocaleMutation = () => {
-  return useMutation(ENABLE_LOCALE, {
-    refetchQueries: [GET_SHOP_LOCALES],
+  return useMutation(ENABLE_LOCALE_MUTATION, {
+    refetchQueries: [GET_SHOP_LOCALES_QUERY],
     awaitRefetchQueries: true
   });
 };
 
 export const useDisableLocaleMutation = () => {
-  return useMutation(DISABLE_LOCALE, {
-    refetchQueries: [GET_SHOP_LOCALES],
+  return useMutation(DISABLE_LOCALE_MUTATION, {
+    refetchQueries: [GET_SHOP_LOCALES_QUERY],
     awaitRefetchQueries: true
   });
 };
 
 export const useUpdateLocaleMutation = () => {
-  return useMutation(UPDATE_LOCALE, {
-    refetchQueries: [GET_SHOP_LOCALES],
+  return useMutation(UPDATE_LOCALE_MUTATION, {
+    refetchQueries: [GET_SHOP_LOCALES_QUERY],
     awaitRefetchQueries: true
   });
 };
 
 const useShopLocalesContext = () => {
-  const { loading, data } = useQuery(GET_SHOP_LOCALES);
+  const { loading, data } = useQuery(GET_SHOP_LOCALES_QUERY);
 
   const locales = React.useMemo(() => {
     const locales = data?.shopLocales || [];
